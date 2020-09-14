cc9ab790630e01144ac39b1d1c87e955844918cd
Orion Ford <39802627+DesignisOrion@users.noreply.github.com>
2020-09-14 14:45:17 -0400

    adding files

---
+++ circle.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.color('blue', 'green')
+turtle.begin_fill()
+turtle.speed(10)
+for i in range(50):
+    turtle.forward(300)
+    turtle.left(170)
+
+turtle.end_fill()
+turtle.done()
